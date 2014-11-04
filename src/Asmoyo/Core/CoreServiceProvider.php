0dfc8bd662b56e2565562401e70c08b42e3d8fc2
antoniputra <akiddcode@gmail.com>
2014-11-04 18:42:35 +0700

    fix asmoyo core structure

--- src/Asmoyo/Core/CoreServiceProvider.php
+++ src/Asmoyo/Core/CoreServiceProvider.php
@@ -12,6 +12,50 @@ class CoreServiceProvider extends ServiceProvider {
 	protected $defer = false;
 
 	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('asmoyo/core');
+		require_once __DIR__.'/../../filters.php';
+		require_once __DIR__.'/../../routes.php';
+	}
+
+	/**
+	 * Bind repositories.
+	 *
+	 * @return  void
+	 */
+	protected function bindings()
+	{
+
+	}
+
+	/**
+	 * Set up the database connection
+	 *
+	 * @return  void
+	 */
+	public function setConnection()
+	{
+		$connection = Config::get('core::database.default');
+
+		if ($connection != 'default')
+		{
+			$asmoyoConfig 	= Config::get('core::database.connections.'.$connection);
+		}
+		else
+		{
+			$connection 	= Config::get('database.default');
+			$asmoyoConfig 	= Config::get('database.connections.'.$connection);
+		}
+
+		Config::set('database.connections.asmoyo', $asmoyoConfig);
+	}
+
+	/**
 	 * Register the service provider.
 	 *
 	 * @return void
---
+++ src/Asmoyo/Core/Models/Base.php
@@ -0,0 +1,11 @@
+<?php namespace Asmoyo\Core\Models;
+
+use \Illuminate\Database\Eloquent\Model;
+
+class Base extends Model
+{
+    public function getConnection()
+    {
+        return static::resolveConnection('asmoyo');
+    }
+}
