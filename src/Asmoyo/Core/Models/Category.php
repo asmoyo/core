0452be72dc3f86753ad7a7b3578f37be9906c4a1
antoniputra <akiddcode@gmail.com>
2014-11-05 00:42:40 +0700

    add eloquent orm data

---
+++ src/Asmoyo/Core/Models/Category.php
@@ -0,0 +1,24 @@
+<?php namespace Asmoyo\Core\Models;
+
+class Category extends Base {
+
+	/**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'categories';
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = array('parent_id', 'title', 'slug', 'description');
+
+
+	public function posts()
+	{
+		return $this->hasMany('Asmoyo\Core\Models\Post');
+	}
+}
