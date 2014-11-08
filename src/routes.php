fed17db332b8f31f057a97e0698b40c917d56778
antoniputra <akiddcode@gmail.com>
2014-11-08 18:53:06 +0700

    fix route category, post

--- src/routes.php
+++ src/routes.php
@@ -2,10 +2,28 @@
 
 $api_url = Config::get('core::asmoyo.api_url_prefix');
 
-Route::group(array('prefix' => $api_url, 'before' => ['auth.basic', 'asmoyo.api']), function() {
+Route::group(array('prefix' => $api_url, 'before' => ['asmoyo.api']), function() {
 	
-	Route::resource('category', 'Asmoyo\Core\Controllers\Api\CategoryApi');
-	Route::resource('post', 'Asmoyo\Core\Controllers\Api\PostApi');
+	Route::resource('category', 'Asmoyo\Core\Controllers\Api\CategoryApi', array(
+		'names' => array(
+			'index'		=> 'apis.category.index',
+			'show'		=> 'apis.category.show',
+			'store' 	=> 'apis.category.store',
+			'update' 	=> 'apis.category.update',
+			'destroy' 	=> 'apis.category.destroy',
+		),
+		'except'	=> ['create', 'edit']
+	));
+	Route::resource('post', 'Asmoyo\Core\Controllers\Api\PostApi', array(
+		'names' => array(
+			'index'		=> 'apis.post.index',
+			'show'		=> 'apis.post.show',
+			'store' 	=> 'apis.post.store',
+			'update' 	=> 'apis.post.update',
+			'destroy' 	=> 'apis.post.destroy',
+		),
+		'except'	=> ['create', 'edit']
+	));
 
 });
 
