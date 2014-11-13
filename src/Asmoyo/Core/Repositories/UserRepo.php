96023246bdbf7308b15c30c7659ca4c73d051e29
antoniputra <akiddcode@gmail.com>
2014-11-14 05:35:39 +0700

    update user repo

--- src/Asmoyo/Core/Repositories/UserRepo.php
+++ src/Asmoyo/Core/Repositories/UserRepo.php
@@ -1,18 +1,45 @@
 <?php namespace Asmoyo\Core\Repositories;
 
 use Asmoyo\Core\Models\User;
+use Auth;
 
 class UserRepo extends BaseRepo {
 	
 	protected $user;
+
+	protected $auth;
 
 	public function __construct(User $user)
 	{
 		$this->user = $user;
 	}
 	
-	public function getAll()
+	public function getPaginate($limit = 10)
 	{
-		return $this->user->all();
+		return $this->user->paginate($limit);
+	}
+
+	public function login($user, $password, $remember = false)
+	{
+		$credentials = [
+			'password' 	=> $password
+		];
+
+		if( ! filter_var($user, FILTER_VALIDATE_EMAIL)) {
+			$credentials['username'] = $user;
+		} else {
+			$credentials['email'] = $user;
+		}
+
+		if (Auth::attempt($credentials, $remember))
+		{
+		    return true;
+		}
+		return false;
+	}
+
+	public function logout()
+	{
+		Auth::logout();
 	}
 }
